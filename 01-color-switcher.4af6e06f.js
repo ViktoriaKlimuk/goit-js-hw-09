cc6679e4fe566e0805c8180514e24d6fc28a785e
ViktoriaKlimuk <126112514+ViktoriaKlimuk@users.noreply.github.com>
2023-12-11 18:49:51 +0000

    Deploying to gh-pages from @ ViktoriaKlimuk/goit-js-hw-09@a675cb5c4189c747715ac446c5085b566db1632e 🚀

--- 01-color-switcher.4af6e06f.js
+++
@@ -1,2 +0,0 @@
-!function(){const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o;function c(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;console.log(`${t} ${Math.random()}}`),n.style.background=t}t.addEventListener("click",(function(t){o=setInterval(c,1e3)})),e.addEventListener("click",(function(t){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.4af6e06f.js.map
