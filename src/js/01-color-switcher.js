14b1871b468899bd8aa586c4a4cb904719ebc802
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-11 20:39:28 +0200

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const start = document.querySelector("button[data-start]");
+const stop = document.querySelector("button[data-stop]");
+const body = document.querySelector("body")
+let timerColor;
 
+
+start.addEventListener("click", handleColor);
+function handleColor(e) {
+    timerColor = setInterval(changeColor, 1000)
+};
+function changeColor() {
+    const color = getRandomHexColor();
+     console.log(`${color} ${Math.random()}}`);
+    body.style.background = color;
+
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+stop.addEventListener("click", handleStop);
+function handleStop(e) {
+    clearInterval(timerColor)
+}
