cb3bad759463d708d1e50f5bd28b7bc7595a3e28
ViktoriaKlimuk <126112514+ViktoriaKlimuk@users.noreply.github.com>
2023-12-12 18:16:13 +0000

    Deploying to gh-pages from @ ViktoriaKlimuk/goit-js-hw-09@4de1bb0cff25699cfef496324d2e741366aa04de 🚀

--- 03-promises.4017238f.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');const e=document.querySelector("button");console.log(e)}();
-//# sourceMappingURL=03-promises.4017238f.js.map
--- 03-promises.62bfe216.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]');const e=document.querySelector("button");console.log(e);
-//# sourceMappingURL=03-promises.62bfe216.js.map
