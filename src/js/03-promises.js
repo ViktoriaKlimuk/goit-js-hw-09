6d6e6a56f14da09f5b0c0137b6f81b566758d2b4
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-12-12 18:44:02 +0200

    third task update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btn = document.querySelector('button');
+console.log(btn);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
